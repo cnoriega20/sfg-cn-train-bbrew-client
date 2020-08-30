7e58158acfadd8ab1243142cf05528ed406f6827
cnoriega20 <c79.noriega@gmail.com>
2020-08-30 11:07:03 -0500

    First commit: Project structure

---
+++ src/main/java/tn/mssvc/client/sfgcntrainbbrewclient/SfgCnTrainBbrewClientApplication.java
@@ -0,0 +1,13 @@
+package tn.mssvc.client.sfgcntrainbbrewclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgCnTrainBbrewClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgCnTrainBbrewClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tn/mssvc/client/sfgcntrainbbrewclient/SfgCnTrainBbrewClientApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.mssvc.client.sfgcntrainbbrewclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgCnTrainBbrewClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

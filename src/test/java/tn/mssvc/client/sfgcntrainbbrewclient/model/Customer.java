45a8bc6616ce9f0a4e4d7c8be49a05dd4fc8cfa7
cnoriega20 <c79.noriega@gmail.com>
2020-08-30 12:13:57 -0500

    Adding Base POJOs

---
+++ src/test/java/tn/mssvc/client/sfgcntrainbbrewclient/model/Customer.java
@@ -0,0 +1,18 @@
+package tn.mssvc.client.sfgcntrainbbrewclient.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Customer {
+
+    private UUID id;
+    private String name;
+}
